50573b844862b73de3465589ff61d46272882151
Guilherme-NL <guilherme.nunes.lopes@gmail.com>
2022-08-06 17:33:33 -0300

    feat: add signup and signin router and controller

---
+++ src/controllers/authController.js
@@ -0,0 +1,96 @@
+import { connection } from "../dbStrategy/postgres.js";
+import { v4 as uuid } from "uuid";
+import joi from "joi";
+import bcrypt from "bcrypt";
+
+export async function signup(req, res) {
+  const user = req.body;
+  console.log(req.body);
+
+  //Validation joi
+  const userSchema = joi.object({
+    name: joi.string().required(),
+    email: joi.string().email().required(),
+    password: joi.required(),
+    confirmPassword: joi.required(),
+  });
+
+  const { error } = userSchema.validate(user);
+
+  if (error || user.password !== user.confirmPassword) {
+    res.sendStatus(422);
+    return;
+  }
+
+  //Email_validation
+  const { rows: checkEmail } = await connection.query(
+    "SELECT * FROM users WHERE email = $1",
+    [user.email]
+  );
+
+  if (checkEmail.length > 0) {
+    res.sendStatus(409);
+    return;
+  }
+
+  const passwordCrypt = bcrypt.hashSync(user.password, 10);
+
+  try {
+    await connection.query(
+      'INSERT INTO users ("name","email","password") VALUES ($1, $2, $3)',
+      [user.name, user.email, passwordCrypt]
+    );
+
+    res.sendStatus(201);
+  } catch {
+    res.sendStatus(400);
+  }
+}
+
+export async function signin(req, res) {
+  const user = req.body;
+
+  //Validation joi
+  const userSchema = joi.object({
+    email: joi.string().email().required(),
+    password: joi.string().required(),
+  });
+
+  const { error } = userSchema.validate(user);
+
+  if (error) {
+    res.sendStatus(422);
+    return;
+  }
+
+  //user validation
+  const { rows: userValidation } = await connection.query(
+    "SELECT * FROM users WHERE email = $1",
+    [user.email]
+  );
+
+  console.log(userValidation);
+
+  const comparePassword = bcrypt.compareSync(
+    user.password,
+    userValidation[0].password
+  );
+
+  if (userValidation.length < 1 || !comparePassword) {
+    return res.sendStatus(401);
+  }
+
+  try {
+    //Token generatioin
+    const token = uuid();
+
+    await connection.query(
+      'INSERT INTO sessions ("userId","token") VALUES ($1, $2)',
+      [userValidation[0].id, token]
+    );
+
+    res.sendStatus(200);
+  } catch {
+    res.sendStatus(400);
+  }
+}
