ce5a61134b8ee073b07679eb519f4d5314761f25
Guilherme-NL <guilherme.nunes.lopes@gmail.com>
2022-08-07 03:43:04 -0300

    feat: add get and delete urls controllers

--- src/controllers/urlsController.js
+++ src/controllers/urlsController.js
@@ -13,6 +13,7 @@ export async function shorten(req, res) {
 
   if (!req.headers || sessions.length < 1) {
     res.sendStatus(401);
+    return;
   }
 
   const { url } = req.body;
@@ -41,6 +42,86 @@ export async function shorten(req, res) {
     );
 
     res.send(body).status(201);
+  } catch {
+    res.sendStatus(400);
+  }
+}
+
+export async function getShort(req, res) {
+  const id = req.params.id;
+
+  try {
+    const { rows: body } = await connection.query(
+      'SELECT id, url, "shortUrl" FROM urls WHERE urls.id = $1',
+      [id]
+    );
+
+    if (body.length < 1) {
+      res.sendStatus(404);
+      return;
+    }
+
+    res.status(200).send(body);
+  } catch {
+    res.sendStatus(400);
+  }
+}
+
+export async function getShortOpen(req, res) {
+  const shortUrl = req.params.shortUrl;
+
+  const { rows: short } = await connection.query(
+    'SELECT * FROM urls WHERE "shortUrl" = $1',
+    [shortUrl]
+  );
+
+  if (short.length < 1) {
+    res.sendStatus(404);
+    return;
+  }
+
+  const newVisitCount = short[0].visitCount + 1;
+  console.log(newVisitCount);
+  await connection.query(
+    'UPDATE urls SET "visitCount" = $1 WHERE "shortUrl" = $2',
+    [newVisitCount, shortUrl]
+  );
+
+  res.redirect(short[0].url);
+}
+
+export async function deleteShort(req, res) {
+  const { authorization } = req.headers;
+  const token = authorization?.replace("Bearer ", "");
+  const id = req.params.id;
+
+  const { rows: sessions } = await connection.query(
+    "SELECT * FROM sessions WHERE token = $1",
+    [token]
+  );
+
+  const { rows: short } = await connection.query(
+    "SELECT * FROM urls WHERE id = $1",
+    [id]
+  );
+
+  if (short.length < 1) {
+    res.sendStatus(404);
+    return;
+  }
+
+  if (
+    !req.headers ||
+    sessions.length < 1 ||
+    short[0].userId !== sessions[0].userId
+  ) {
+    res.sendStatus(401);
+    return;
+  }
+
+  try {
+    await connection.query("DELETE FROM urls WHERE id = $1", [id]);
+    res.sendStatus(204);
   } catch {
     res.sendStatus(400);
   }
--- src/routes/urlsRouter.js
+++ src/routes/urlsRouter.js
@@ -1,8 +1,19 @@
-import { shorten } from "../controllers/urlsController.js";
+import {
+  shorten,
+  getShort,
+  getShortOpen,
+  deleteShort,
+} from "../controllers/urlsController.js";
 import { Router } from "express";
 
 const router = Router();
 
 router.post("/urls/shorten", shorten);
+
+router.get("/urls/:id", getShort);
+
+router.get("/urls/open/:shortUrl", getShortOpen);
+
+router.delete("/urls/:id", deleteShort);
 
 export default router;
